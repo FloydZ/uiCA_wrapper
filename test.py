989664abd508ef5161ad9783b240d2dea563575a
Floyd Zweydinger <floyd.zweydinger@rub.de>
2023-01-14 22:35:51 +0100

    added second simple x64 test

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from uica_wrapper import uiCA_Wrapper
 
+
 def test1():
     test = "l: add rax, rbx; add rbx, rax; dec r15; jnz l"
     o = uiCA_Wrapper(test)
@@ -10,5 +11,19 @@ def test1():
     assert t == 2.
 
 
+def test2():
+    a = ["lea rax, -1040[rbp]", 
+         "mov rsi, rax",
+         "lea rax, .LC0[rip]",
+         "mov rdi, rax"]
+    test = "\n".join(a)
+    o = uiCA_Wrapper(test)
+    t = o.run()
+
+    # print(t)
+    assert t == 1.25
+
+
 if __name__ == "__main__":
     test1()
+    test2()
