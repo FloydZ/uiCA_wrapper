364296492f155cf32f4438162ff3388002665b63
Floyd Zweydinger <zweydfg8+github@rub.de>
2025-05-15 11:59:49 +0200

    cleanup

--- src/machine_code_analyzer/__init__.py
+++ src/machine_code_analyzer/__init__.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 """ main imports """
 
 from .llvm_mca_wrapper import LLVM_MCA
-# TODO currently not working from .uica_wrapper import uiCA_Wrapper
--- src/machine_code_analyzer/test/test.py
+++ src/machine_code_analyzer/test/test.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """ tests for uica """
 
-from machine_code_analyzer.uica_wrapper import uiCA_Wrapper
+from machine_code_analyzer.uica_wrapper import uiCA_wrapper
 
 
 def test1():
+    """ simple test, if this fails something is really off """
     test = "l: add rax, rbx; add rbx, rax; dec r15; jnz l"
-    o = uiCA_Wrapper(test)
+    o = uiCA_wrapper(test)
     t = o.run()
 
     # print(t)
@@ -14,12 +15,13 @@ def test1():
 
 
 def test2():
+    """ simple test, if this fails something is really off """
     a = ["lea rax, -1040[rbp]", 
          "mov rsi, rax",
          "lea rax, .LC0[rip]",
          "mov rdi, rax"]
     test = "\n".join(a)
-    o = uiCA_Wrapper(test)
+    o = uiCA_wrapper(test)
     t = o.run()
 
     # print(t)
